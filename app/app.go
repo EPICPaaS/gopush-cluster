199f10bf0f7438bc24bb1175059338f8b12aca2a
MrHu <229712806@qq.com>
2014-08-19 13:47:00 +0800

    哈哈哈

--- app/app.go
+++ app/app.go
@@ -1,15 +1,5 @@
 package app
 
-import (
-	"encoding/json"
-	myrpc "github.com/EPICPaaS/gopush-cluster/rpc"
-	"github.com/golang/glog"
-	"io/ioutil"
-	"net/http"
-	"strings"
-	"time"
-)
-
 type device struct{}
 
 // 移动端接口
