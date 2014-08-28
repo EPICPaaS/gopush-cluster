69c038a5cf9e9ee83e7905959fc085fb2a18f282
Liang Ding <DL88250@gmail.com>
2014-08-28 17:37:56 +0800

    app 部分建表 SQL 脚本

--- sql/install.sql
+++ sql/install.sql
@@ -1,9 +1,5 @@
 delimiter $$
 
-CREATE DATABASE `appmsgsrv` /*!40100 DEFAULT CHARACTER SET utf8 */$$
-
-delimiter $$
-
 CREATE TABLE `client` (
   `id` bigint(20) NOT NULL,
   `user_id` bigint(20) DEFAULT NULL,
@@ -17,11 +13,38 @@ CREATE TABLE `client` (
 
 delimiter $$
 
-CREATE TABLE `group` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `max_member` int(11) DEFAULT NULL,
+CREATE TABLE `org` (
+  `id` varchar(64) NOT NULL,
+  `name` varchar(120) NOT NULL DEFAULT '' COMMENT '编码',
+  `short_name` varchar(60) NOT NULL DEFAULT '',
+  `parent_id` varchar(64) NOT NULL DEFAULT '',
+  `location` varchar(256) NOT NULL DEFAULT '',
+  `tenant_id` varchar(64) NOT NULL DEFAULT '',
+  `sort` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `org_user` (
+  `id` varchar(64) NOT NULL,
+  `org_id` varchar(64) NOT NULL DEFAULT '',
+  `user_id` varchar(64) NOT NULL DEFAULT '',
+  `sort` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `qun` (
+  `id` varchar(64) NOT NULL,
+  `creator_id` varchar(64) NOT NULL DEFAULT '',
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `max_member` int(11) NOT NULL DEFAULT '0',
+  `avatar` varchar(255) NOT NULL DEFAULT '',
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -30,10 +53,25 @@ CREATE TABLE `group` (
 
 delimiter $$
 
-CREATE TABLE `group_user` (
-  `id` bigint(20) NOT NULL,
-  `group_id` bigint(20) DEFAULT NULL,
-  `user_id` bigint(20) DEFAULT NULL,
+CREATE TABLE `qun_user` (
+  `id` varchar(64) NOT NULL,
+  `qun_id` varchar(64) NOT NULL DEFAULT '',
+  `user_id` varchar(64) NOT NULL DEFAULT '',
+  `sort` int(11) NOT NULL DEFAULT '0',
+  `role` int(11) NOT NULL DEFAULT '0' COMMENT '0：群主；1：管理员；2：成员',
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `tenant` (
+  `id` varchar(64) NOT NULL,
+  `code` varchar(64) NOT NULL DEFAULT '',
+  `name` varchar(128) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL DEFAULT '0',
   `created` datetime DEFAULT NULL,
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -43,39 +81,22 @@ CREATE TABLE `group_user` (
 delimiter $$
 
 CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `nickname` varchar(45) DEFAULT NULL,
-  `avatar` varchar(45) DEFAULT NULL,
-  `gender` char(1) DEFAULT NULL,
-  `signature` varchar(255) DEFAULT NULL,
-  `name_py` varchar(45) DEFAULT NULL,
-  `name_quanpin` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
-  `province` varchar(45) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `alias` varchar(45) DEFAULT NULL,
-  `rand` varchar(45) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
-  `key` varchar(32) DEFAULT NULL,
-  `level` int(11) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `updated` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
-
-delimiter $$
-
-CREATE TABLE `user_user` (
-  `id` bigint(20) NOT NULL,
-  `from_user_id` bigint(20) DEFAULT NULL,
-  `to_user_id` bigint(20) DEFAULT NULL,
-  `remark_name` varchar(45) DEFAULT NULL,
-  `remark_name_py` varchar(45) DEFAULT NULL,
-  `remark_name_quanpin` varchar(255) DEFAULT NULL,
-  `created` datetime DEFAULT NULL,
-  `updated` datetime DEFAULT NULL,
+  `id` varchar(64) NOT NULL,
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `nickname` varchar(45) NOT NULL DEFAULT '',
+  `avatar` varchar(45) NOT NULL DEFAULT '',
+  `name_py` varchar(45) NOT NULL DEFAULT '',
+  `name_quanpin` varchar(255) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `rand` int(11) NOT NULL DEFAULT '0',
+  `password` varchar(32) NOT NULL DEFAULT '',
+  `tenant_id` varchar(64) NOT NULL DEFAULT '',
+  `level` int(11) NOT NULL DEFAULT '0',
+  `email` varchar(64) NOT NULL DEFAULT '',
+  `mobile` varchar(11) NOT NULL DEFAULT '',
+  `area` varchar(128) NOT NULL DEFAULT '',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
