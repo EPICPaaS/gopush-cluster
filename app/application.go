35938b08b3b75f77d619664fdc6092e59836a244
Liang Ding <DL88250@gmail.com>
2014-09-02 13:27:12 +0800

    w

---
+++ app/application.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	"time"
+)
+
+const (
+	// 根据 id 查询应用记录.
+	SelectApplicationById = "SELECT * FROM `app` WHERE id = ?"
+)
+
+// 应用结构.
+type application struct {
+	Id      string    `json:"id"`
+	Name    string    `json:"name"`
+	Token   string    `json:"token"`
+	Type    int       `json:"type"`
+	Status  int       `json:"status"`
+	Sort    int       `json:"sort"`
+	Level   int       `json:"level"`
+	Created time.Time `json:"created"`
+	Updated time.Time `json:"updated"`
+}
+
+// 数据库中根据 id 查询应用记录.
+func getApplication(appId string) (*application, err) {
+	row := MySQL.QueryRow(SelectApplicationById, appId)
+
+	application := application{}
+	if err := row.Scan(&application.Id, &application.Name, &application.Token, &application.Type, &application.Status,
+		&application.Sort, &application.Level, &application.Created, &application.Updated); err != nil {
+		glog.Error(err)
+
+		return nil, err
+	}
+
+	return &application, err
+}
